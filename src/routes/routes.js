43683edab2cef7bf438db772ae0b41a20ebc915a
SpawnSkull <saposuper_14@hotmail.com>
2022-02-06 22:53:36 -0600

    Vista Landing creada

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,20 @@ import Access from "@/pages/Layout/LoginLayout.vue";
 import Login from "@/components/views/LoginView.vue";
 
 const routes = [
+    
+    {
+        path: "/",
+        component:() => import("@/pages/Layout/Landing.vue"),
+        redirect: "/landing",
+        children: [
+            {
+                path: "landing",
+                name: "Landing",
+                component:() => import("@/pages/Layout/Landing.vue") ,
+            }
+        ]
+
+    },
     {
         path: "/",
         component: Access,
